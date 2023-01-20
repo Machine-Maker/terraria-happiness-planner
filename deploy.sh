ee4deaeb45fefad5e48ce8762d4540395864ec7b
Jake Potrebic <jake.m.potrebic@gmail.com>
2023-01-20 14:46:12 -0800

    add .nojekyll file to deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ pnpm generate
 cd dist || exit
 git init
 printf ".DS_Store\n" > .gitignore
+touch .nojekyll
 git add -A
 git commit "deployed: $(date)}"
 git push -f git@github.com:Machine-Maker/terraria-npc-happiness.git HEAD:gh-pages
