c717fea5884fdea31f07ced572f7773e8569911a
Jake Potrebic <jake.m.potrebic@gmail.com>
2023-01-20 14:56:03 -0800

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,12 @@
 pnpm install
 pnpm generate
+commit_msg=$(git log --format=reference -1)
 cd dist || exit
 git init
+git config --local user.email "action@github.com"
+git config --local user.name "GitHub Action"
 printf ".DS_Store\n" > .gitignore
 touch .nojekyll
 git add -A
-git commit -m "deployed: $(date)}"
+git commit -m "$commit_msg"
 git push -f git@github.com:Machine-Maker/terraria-npc-happiness.git HEAD:gh-pages
