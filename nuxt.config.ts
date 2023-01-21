10468e57eaa3939ad111f51a1061f50e3c8a308f
Jake Potrebic <jake.m.potrebic@gmail.com>
2023-01-20 21:48:46 -0800

    fix safari favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,7 +23,6 @@ export default defineNuxtConfig({
         { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
         { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
         { rel: "manifest", href: "/site.webmanifest" },
-        { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#F38C4F" },
       ],
     },
   },
